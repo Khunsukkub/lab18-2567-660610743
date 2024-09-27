3380e689b378cbf1bec58e9c6cf935ca99d3a889
Khun <khunsuekthai_bua@cmu.ac.th>
2024-09-27 15:17:01 +0700

    Updated

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -5,7 +5,6 @@ import { read } from "fs";
 
 //POST /api/user/login
 export const POST = async (request: NextRequest) => {
-  readDB
   const body = await request.json();
   const { username, password } = body;
 
